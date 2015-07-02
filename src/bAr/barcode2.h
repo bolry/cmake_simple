cb4c727f577460ec7d3191975a0627dad8d12f64
Bo Rydberg <github@rydb3rg.com>
2015-07-02 22:28:01 +0200

    Create barcode2.h

---
+++ src/bAr/barcode2.h
@@ -0,0 +1,17 @@
+/*
+ * barcode2.h
+ *
+ *  Created on: Jun 9, 2015
+ *      Author: bo
+ */
+
+#ifndef BARCODE2_H_
+#define BARCODE2_H_
+
+class Barcode2 {
+public:
+	Barcode2();
+	void DoBarCode2Stuff();
+};
+
+#endif /* BARCODE2_H_ */
