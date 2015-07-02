236473ee0a0e6910302878f6907218cb24f31c17
Bo Rydberg <github@rydb3rg.com>
2015-07-02 22:31:43 +0200

    Create foolish2.h

---
+++ src/fOo/foolish2.h
@@ -0,0 +1,17 @@
+/*
+ * foolish2.h
+ *
+ *  Created on: Jun 9, 2015
+ *      Author: bo
+ */
+
+#ifndef FOOLISH2_H_
+#define FOOLISH2_H_
+
+class foolish2 {
+public:
+	foolish2();
+	void DoFoolishStuff();
+};
+
+#endif /* FOOLISH2_H_ */
