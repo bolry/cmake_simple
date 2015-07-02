86b38bcc1af29bd8bb606fa40aa3c93323d22814
Bo Rydberg <github@rydb3rg.com>
2015-07-02 22:25:05 +0200

    Create Aron_main.cpp

---
+++ src/App/Aron_main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "barcode.h"
+#include "barcode2.h"
+#include "foolish.h"
+#include "foolish2.h"
+
+int main()
+{
+	FooCode();
+	BarCode();
+	Barcode2 bc2;
+	bc2.DoBarCode2Stuff();
+	foolish2 f2;
+	f2.DoFoolishStuff();
+}
